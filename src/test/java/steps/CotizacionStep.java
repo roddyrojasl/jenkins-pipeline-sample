e5ea42c01cfa521e01e257a0d05f0e1e6cc0ee23
Roddy Illamaru <roddyorlandorl@gmail.com>
2025-03-20 11:42:02 -0500

    Initial commit

---
+++ src/test/java/steps/CotizacionStep.java
@@ -0,0 +1,22 @@
+package steps;
+
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.actors.OnlineCast;
+
+
+public class CotizacionStep {
+
+//    @Before
+//    public void setUp(){
+//        OnStage.setTheStage(new OnlineCast());
+//    }
+
+
+    @Given("Cargo pagina de cotizacion de SOAT")
+    public void CargarPagina(){
+
+
+    }
+}
---
+++ src/test/resources/features/Cotizacion.feature
@@ -0,0 +1,7 @@
+Feature: Cotizar SOAT
+
+  Background: cargo pagina de cotizacion de SOAT
+    Given que cargo la pagina de cotizacion
+
+  Scenario: Cotizar SOAT de cliente
+    When
